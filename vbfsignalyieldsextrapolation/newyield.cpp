5ae378b276316ee95e993a8d57cb9bef8d13595e
Patrick <pdunne@cern.ch>
2014-02-21 18:23:18 +0100

    fixing interpolator for ggh process

--- vbfsignalyieldsextrapolation/newyield.cpp
+++ vbfsignalyieldsextrapolation/newyield.cpp
@@ -1,12 +1,11 @@
 int newyield(string inputfile){
-{
 
   //GET XS AND YIELD INFO ABOUT INPUT MASS POINTS
   std::vector<double> *inmass = new std::vector<double>();
   std::vector<double> *inxs = new std::vector<double>();
   std::vector<double> *inyield = new std::vector<double>();
 
-  std::ifstream inputinfofile("inputinfo.txt");  
+  std::ifstream inputinfofile((inputfile+"inputinfo.txt").c_str());//"inputinfo.txt");  
   std::string line;
 
   while(std::getline(inputinfofile,line)){
@@ -20,7 +19,7 @@ int newyield(string inputfile){
   }
 
   //GET XS INFO ABOUT OUTPUT MASS POINTS
-  std::ifstream outputxsfile("outputxsinfo.txt");
+  std::ifstream outputxsfile(("output"+inputfile+"xsinfo.txt").c_str());
 
   std::vector<double> *outmass = new std::vector<double>();
   std::vector<double> *outxs = new std::vector<double>();
@@ -37,7 +36,7 @@ int newyield(string inputfile){
   std::vector<double> *inyieldoverxs = new std::vector<double>();
   for(int i=0;i<inmass->size();i++){
     inyieldoverxs->push_back(inyield->at(i)/inxs->at(i));
-    //std::cout << inyield->at(i) <<" "<< inxs->at(i)<<" " << inyieldoverxs->at(i)<<std::endl;
+    std::cout << inyield->at(i) <<" "<< inxs->at(i)<<" " << inyieldoverxs->at(i)<<std::endl;
   }
 
   //MAKE TGRAPHS
