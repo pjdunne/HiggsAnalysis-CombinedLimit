a5d381a63d587df3abe23db479187e5e8d04d475
Patrick <pdunne@cern.ch>
2016-03-12 09:19:45 +0000

    long overdue commits

--- moriond2016invcomb/hinvcombiner.sh
+++ moriond2016invcomb/hinvcombiner.sh
@@ -3,7 +3,38 @@ cl=0.95
 for dir in 125
   do cd $dir
   echo $dir:
-  #combineCards.py -S vbfhinv*.txt zeehinv*.txt zmmhinv*.txt zhinv_Zbb*.txt combined_hinv.txt >hinvcombinedcard.txt
+  echo "VBF only:"
+  combineCards.py -S vbfhinv*.txt > vbfcombinedcard.txt
+  combine -M Asymptotic --cl $cl -m $dir -n VBF vbfcombinedcard.txt
+
+  # echo "ggH only:"
+  # combineCards.py monojet_*.txt > gghcombinedcard.txt
+  # combine -M Asymptotic --cl $cl -m $dir -n ggH gghcombinedcard.txt
+
+  echo "VH only:"
+#  combineCards.py -S monov_*.txt zh${dir}*.text > vhcombinedcard.txt
+  combineCards.py -S zh${dir}*.text > vhcombinedcard.txt
+  combine -M Asymptotic --cl $cl -m $dir -n VH vhcombinedcard.txt
+
+  # echo "ZH+VBF only:"
+  # combineCards.py -S vbfhinv*.txt zh${dir}*.text > vbfzhcombinedcard.txt
+  # combine -M Asymptotic --cl $cl -m $dir -n vbfzh vbfzhcombinedcard.txt
+
+  echo "Combined:"
+  combineCards.py -S vbfhinv*.txt zh${dir}*.text >hinvcombinedcard.txt
+  combine -M Asymptotic --cl $cl -m $dir -n Comb hinvcombinedcard.txt
+
+  # echo "Combined:"
+  # combineCards.py -S vbfhinv*.txt zh${dir}*.text monov_*.txt monojet_*.txt >hinvcombinedcard.txt
+  # combine -M Asymptotic --cl $cl -m $dir -n Comb hinvcombinedcard.txt
+  cd ..
+done
+hadd -f allcombineresults.root */higgsCombineTest.Asymptotic.mH*.root
+
+
+
+
+#8 TeV lines
 #  combineCards.py -S vbfhinv*.txt zeehinv*.txt zmmhinv*.txt combined_hinv.txt >hinvcombinedcardnozbb.txt
   #combine -M Asymptotic  -m $dir hinvcombinedcard.txt | tee combineoutput${dir}.log
 #  combineCards.py -S vbfhinv*.txt zeehinv*.txt zmmhinv*.txt zhinv_Zbb*.txt combined_boosted_hinv.txt combined_monojet_hinv.txt >hinvcombinedcardnoresolved.txt
@@ -12,9 +43,3 @@ for dir in 125
 #  combine -M Asymptotic --cl $cl -m $dir -n VH vhcombinedcard.txt | tee combineoutput${dir}.log
 #  combine -M Asymptotic --cl $cl -m $dir -n VBF vbfhinv_125_8TeV.txt | tee combineoutput${dir}.log
 #  combine -M Asymptotic --cl $cl -m $dir -n ggH combined_monojet_hinv.txt
-
-  combineCards.py vbfhinv*.txt #zllhinv*.txt >hinvcombinedcard.txt
-#  combine -M Asymptotic --cl $cl -m $dir -n Comb hinvcombinedcard.txt
-  cd ..
-done
-hadd -f allcombineresults.root */higgsCombineTest.Asymptotic.mH*.root
